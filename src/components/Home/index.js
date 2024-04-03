246875d48cd3b9e2144a1a769772779aba2becbe
Urvi Patel <urvipatel922@gmail.com>
2024-04-03 18:37:30 -0400

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -51,7 +51,7 @@ const Home = () => {
                         />
                     </h1>
                     <h2>
-                        Full Stack Web Developer / Data Analysis
+                        Learner of life / Seeker of purpose
                     </h2>
                     <Link to="/contact" className="flat-button">
                         CONTACT ME
